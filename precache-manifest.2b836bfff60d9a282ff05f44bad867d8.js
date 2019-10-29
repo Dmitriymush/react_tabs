c0715fd2d63d265eeb18880db42461e8ce7c7078
Dmitriymush <powder322@gmail.com>
2019-10-29 18:32:37 +0200

    Updates

---
+++ precache-manifest.2b836bfff60d9a282ff05f44bad867d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5f5d5e4a9d80403d5ed09e107074f5",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "f3511d93096d242317cc",
+    "url": "/react_tabs/static/css/main.be5d3945.chunk.css"
+  },
+  {
+    "revision": "f730d0c4bbf0eeb965aa",
+    "url": "/react_tabs/static/js/2.a717a1b1.chunk.js"
+  },
+  {
+    "revision": "f3511d93096d242317cc",
+    "url": "/react_tabs/static/js/main.44b4672c.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
